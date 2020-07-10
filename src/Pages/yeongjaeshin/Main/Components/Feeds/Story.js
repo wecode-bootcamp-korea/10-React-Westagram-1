7b8f697445744a6e468500851addec11b6787c09
Yeongjae-Shin <syj9484@gmail.com>
2020-07-10 18:42:00 +0900

    [신영재] Refactor: 컴포넌트 세분화

---
+++ src/Pages/yeongjaeshin/Main/Components/Feeds/Story.js
@@ -0,0 +1,101 @@
+import React from "react";
+import "./Story.scss";
+
+class Story extends React.Component {
+  render() {
+    return (
+      <div className="feed-story">
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/xcBDWWXv1hr" />
+            </div>
+            <span className="story-profile-id">realisshoman</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/jgstnMkAhEf" />
+            </div>
+            <span className="story-profile-id">nasungcityboy</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/pALfma6bNV8" />
+            </div>
+            <span className="story-profile-id">thisloop</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/KU7jRuBbyxc" />
+            </div>
+            <span className="story-profile-id">kimehwa</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/6NxEpTSJAmP" />
+            </div>
+            <span className="story-profile-id">thequiett</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/0kf7Gqta25e" />
+            </div>
+            <span className="story-profile-id">nucksal</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/mGNtqeNEcq" />
+            </div>
+            <span className="story-profile-id">deepflow39</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/al02kpbMq5P" />
+            </div>
+            <span className="story-profile-id">donmills1988</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/5bFiCwuIamn" />
+            </div>
+            <span className="story-profile-id">bigoneisthename</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/YHjtuiHSOZM" />
+            </div>
+            <span className="story-profile-id">thisisneverthat</span>
+          </li>
+        </ul>
+        <ul>
+          <li className="story-list">
+            <div className="story-profile-img">
+              <img alt="story" src="http://bitly.kr/tTq5b0KwTMY" />
+            </div>
+            <span className="story-profile-id">lostmanagementcities</span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Story;
